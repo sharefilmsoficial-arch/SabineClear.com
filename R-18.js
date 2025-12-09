3889ad7edd21b96630b140eda1e7a7d6087e2b5e
ShareFilmsOficial <sharefilmsoficial@gmail.com>
2025-12-09 16:36:03 -0600

    Add files via upload

---
+++ R-18.js
@@ -0,0 +1,19 @@
+const sampleTrailer = "https://www.w3schools.com/html/mov_bbb.mp4"; // Tráiler genérico temporal
+
+const MOVIES = [
+  {
+    id: "",
+    title: "La hermanastra Fea",
+    year: 20,
+    duration: "1h 56min",
+    rating: "R-18",
+    genres: [""],
+    description: "",
+    trailerUrl: sampleTrailer,
+    image: "",
+    movie: ""
+  }
+];
+
+// (Opcional) Exponerlo globalmente
+window.MOVIES = MOVIES;
