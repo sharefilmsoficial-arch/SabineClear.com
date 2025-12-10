78ee2f536db3ffe55265dfc4ecfdd4c0184dc05c
ShareFilmsOficial <sharefilmsoficial@gmail.com>
2025-12-09 18:50:27 -0600

    Update R-18.js

--- R-18.js
+++ R-18.js
@@ -11,10 +11,11 @@ const MOVIES = [
     description: "",
     trailerUrl: sampleTrailer,
     image: "R-18/images/the-ugly-stepsister.webp",
-    poster: "R-18/posters/the-ugly-stepsister.webp"
+    poster: "R-18/posters/the-ugly-stepsister.webp",
     movie: ""
   }
 ];
 
 // (Opcional) Exponerlo globalmente
 window.MOVIES = MOVIES;
+
