28530aa0cbd99054a45918d5afb7582989cf028c
ShareFilmsOficial <sharefilmsoficial@gmail.com>
2026-01-07 17:33:52 -0600

    Add files via upload

---
+++ amazon-prime/publicidad.js
@@ -0,0 +1,32 @@
+const PRIME_MOVIES = [
+  {
+    id: "betty-la-fea-tp-2",
+    title: "Betty La Fea: La Historia Continúa - Temporada 2",
+    image: "amazon-prime/images/Betty-La-Fea.jpg",
+    link:"https://www.primevideo.com/-/es/detail/Betty-La-Fea-La-Historia-Continua/0JP19B4WQVKERE4RCNHV94A5FK"
+  },
+  {
+    id: "codigo-traje-rojo",
+    title: "Código: Traje Rojo",
+    image: "amazon-prime/images/codigo-traje-rojo.png",
+    link:"https://www.primevideo.com/-/es/detail/C%C3%B3digo-Traje-Rojo/0RNA7FGRLKHSV1BNM01TY8Q33T"
+  },
+  {
+    id: "invencible",
+    title: "Invencible",
+    image: "amazon-prime/images/invencible.png",
+    link:"https://www.primevideo.com/dp/0RI8K407MG2FJ6F2GBY1PQTIGX?ref_=dvm_pds_tit_mx_sw_s_g_mkw_p414-kw150032-cr293687-c&mrntrk=go_cmp-20187812366_adg-165290595348_ad-708258376991_kwd-1188992179599_dev-c_ext-&gclsrc=aw.ds&gad_source=1&gad_campaignid=20187812366&gbraid=0AAAAApd-esmopGP94gDKzgaJ7PKyT4RKQ&gclid=CjwKCAiAmePKBhAfEiwAU3Ko3G-0VAEyDzgBd12Rg3SRK4ObaPvxVqJiiGfn6rHTEr1JGH_oSDFF0RoCcgsQAvD_BwE"
+  },
+  {
+    id: "riesgo-bajo-cero",
+    title: "Riesgo Bajo Cero",
+    image: "amazon-prime/images/riesgo-bajo-cero.png",
+    link:"https://www.primevideo.com/dp/0JR37N49GGQ9NMX1JJ20GRH4CT?ref_=dvm_pds_tit_mx_sw_s_g_mkw_p414-kw1305236-cr1305753-c&mrntrk=go_cmp-20849098307_adg-187367515840_ad-772350156133_kwd-2438041774950_dev-c_ext-&gclsrc=aw.ds&gad_source=1&gad_campaignid=20849098307&gbraid=0AAAAApd-eskXY2B9XtwqZdZIF2b5peUE2&gclid=CjwKCAiAmePKBhAfEiwAU3Ko3Pb3EJfo2Nx-QTCEXXCW509vFyckzkacfnLnHd0mMPxVzjDaTSQokBoCY00QAvD_BwE"
+  },
+  {
+    id: "the-good-doctor",
+    title: "The Good Doctor",
+    image: "amazon-prime/images/the-good-doctor.png",
+    link:"https://www.primevideo.com/-/es/detail/The-Good-Doctor/0OO3EI0WX74I0JQ44KM0DGWHPT"
+  }
+]
