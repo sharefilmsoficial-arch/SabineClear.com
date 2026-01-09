cc71c4edfcae45a5ae6118e172df9ee0fbe1df82
ShareFilmsOficial <sharefilmsoficial@gmail.com>
2026-01-09 17:21:33 -0600

    Update publicidad.js

--- amazon-prime/publicidad.js
+++ amazon-prime/publicidad.js
@@ -19,7 +19,7 @@ const PRIME_MOVIES = [
   },
   {
     id: "riesgo-bajo-cero",
-    title: "Riesgo Bajo Cero",
+    title: "Riesgo Bajo Cero: Venganza",
     image: "amazon-prime/images/riesgo-bajo-cero.png",
     link:"https://www.primevideo.com/dp/0JR37N49GGQ9NMX1JJ20GRH4CT?ref_=dvm_pds_tit_mx_sw_s_g_mkw_p414-kw1305236-cr1305753-c&mrntrk=go_cmp-20849098307_adg-187367515840_ad-772350156133_kwd-2438041774950_dev-c_ext-&gclsrc=aw.ds&gad_source=1&gad_campaignid=20849098307&gbraid=0AAAAApd-eskXY2B9XtwqZdZIF2b5peUE2&gclid=CjwKCAiAmePKBhAfEiwAU3Ko3Pb3EJfo2Nx-QTCEXXCW509vFyckzkacfnLnHd0mMPxVzjDaTSQokBoCY00QAvD_BwE"
   },
@@ -28,5 +28,29 @@ const PRIME_MOVIES = [
     title: "The Good Doctor",
     image: "amazon-prime/images/the-good-doctor.png",
     link:"https://www.primevideo.com/-/es/detail/The-Good-Doctor/0OO3EI0WX74I0JQ44KM0DGWHPT"
+  },
+  {
+    id: "hasta-la-madre-de-la-navidad",
+    title: "Hasta La Madre De La Navidad",
+    image: "amazon-prime/images/hasta-la-madre-de-la-navidad.png",
+    link:"https://www.primevideo.com/-/es/detail/Hasta-la-madre-de-la-Navidad/0TZMY9UJAY5B054IV2V0KQH4Z5"
+  },
+  {
+    id: "hazbin-hotel-TP2",
+    title: "Hazbin Hotel - Segunda Temporada",
+    image: "amazon-prime/images/hazbin-hotel-TP2.png",
+    link:"https://www.primevideo.com/-/es/detail/Hotel-Hazbin/0K67FVL3VHXNTE3JST8GXWOOFG"
+  },
+  {
+    id: "la-idea-de-ti",
+    title: "La Idea De Ti",
+    image: "amazon-prime/images/la-idea-de-ti.png",
+    link:"https://www.primevideo.com/dp/0NRT15S2XG06SG5HBV5NQAW3E3?ref_=dvm_pds_tit_mx_sw_s_g_mkw_p414-kw233264-cr297949-c&mrntrk=go_cmp-20191545256_adg-174171677165_ad-735645281718_kwd-2298148311090_dev-c_ext-&gclsrc=aw.ds&gad_source=1&gad_campaignid=20191545256&gbraid=0AAAAApd-esmogN04nc6w8OU4WeDNkg638&gclid=Cj0KCQiAyP3KBhD9ARIsAAJLnnYR8KiO3uZekftxT0Q1yNozyaGC2n8Ez9z2gEcdVKA7OB1CZ3i-JJIaAuz7EALw_wcB"
+  },
+  {
+    id: "un-dia-fuera-de-control",
+    title: "Un Día Fuera De Control",
+    image: "amazon-prime/images/un-dia-fuera-de-control.png",
+    link:"https://www.primevideo.com/-/es/detail/Un-d%C3%ADa-fuera-de-control/0P2NNN5ML0IIXVF9I1DQ7VJY8Y"
   }
 ]
