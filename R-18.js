94366ad25f46aeff815e613eff8eec89e79c2e45
ShareFilmsOficial <sharefilmsoficial@gmail.com>
2026-04-10 22:08:55 -0600

    Update R-18.js

--- R-18.js
+++ R-18.js
@@ -26,7 +26,19 @@ const MOVIES = [
     image: "R-18/images/quienes-son-los-millers.webp",
     post: "R-18/posters/quienes-son-los-millers.webp",
     movie: ""
-  }
+  },
+  {
+    id: "el-perfume-la-historia-de-un-asesinato",
+    title: "El Perfume - La Historia de un Asesinato",
+    year: 2013,
+    duration: "1h ",
+    rating: "R-18",
+    genres: ["Comedia","Aventura","Crimen"],
+    description: "David, un distribuidor de marihuana de poca monta,aprende por las malas que ninguna buena acción queda sin castigo. Al intentar ayudar a unos jóvenes, es sorprendido por unos criminales, perdiendo su dinero y producto. David está endeudado con su proveedor y para solucionarlo, debe ir a México para recoger el nuevo cargamento. Para lograr la misión, David idea un plan infalible: reúne a una familia falsa y la sube a una casa rodante para dirigirse al sur durante un fin de semana salvaje.",
+    trailerUrl: sampleTrailer,
+    image: "R-18/images/quienes-son-los-millers.webp",
+    post: "R-18/posters/quienes-son-los-millers.webp",
+    movie: "https://drive.google.com/file/d/14PZN2XzVBriEHj2mrA635U8HLoI1ack2/preview"
 ];
 
 // (Opcional) Exponerlo globalmente
